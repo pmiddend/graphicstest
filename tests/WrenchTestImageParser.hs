97f8612b9842c8159019addbb712510e19baf78a
Philipp Middendorf <pmidden@secure.mailbox.org>
2015-05-24 14:54:17 +0200

    Add one more test for the image parser

--- tests/WrenchTestImageParser.hs
+++ tests/WrenchTestImageParser.hs
@@ -4,7 +4,14 @@ module WrenchTestImageParser(wrenchTestImageParser) where
 import Test.HUnit(Test(..),(@?=))
 import Wrench.ImageParser
 import Wrench.Rectangle
+import Wrench.Animation
 import Linear.V2
 
 wrenchTestImageParser :: Test
-wrenchTestImageParser = TestCase (readImageDataFromText ("car=0,1,30,60") @?= [DataLineImage ("car",rectangleFromPoints (V2 0 1) (V2 30 60))])
+wrenchTestImageParser = TestList [wrenchTestImageParserImage,wrenchTestImageParserAnims]
+
+wrenchTestImageParserImage :: Test
+wrenchTestImageParserImage = TestCase (readImageDataFromText ("car=0,1,30,60") @?= [DataLineImage ("car",rectangleFromPoints (V2 0 1) (V2 30 60))])
+
+wrenchTestImageParserAnims :: Test
+wrenchTestImageParserAnims = TestCase (readImageDataFromText ">player_walk=250|pwr0,pwr1" @?= [DataLineAnim ("player_walk",Animation 250 ["pwr0","pwr1"])])
