8741e77c8a114d7ca128bdea577a58b45e524a4a
Philipp Middendorf <pmidden@secure.mailbox.org>
2015-05-25 13:15:51 +0200

    Add animLifetime and fromMilliseconds

--- src/Wrench/Animation.hs
+++ src/Wrench/Animation.hs
@@ -2,8 +2,12 @@ module Wrench.Animation where
 
 import Wrench.ImageId
 import ClassyPrelude
+import Wrench.Time
 
 data Animation = Animation {
   animFrameSwitch :: Int,
   animFrames      :: [ImageId]
   } deriving(Eq,Show)
+
+animLifetime :: Animation -> TimeDelta
+animLifetime a = fromMilliseconds (fromIntegral (animFrameSwitch a * length (animFrames a)))
--- src/Wrench/Time.hs
+++ src/Wrench/Time.hs
@@ -8,6 +8,7 @@ module Wrench.Time(
   , fromSeconds
   , toSeconds
   , plusDuration
+  , fromMilliseconds
 ) where
 
 import           System.Clock (Clock (Monotonic), TimeSpec (TimeSpec), getTime)
@@ -33,6 +34,9 @@ plusDuration t d = TimeTicks (_timeTicks t + floor (_timeDelta d / nanoSecondsDi
 
 fromSeconds :: Double -> TimeDelta
 fromSeconds = TimeDelta
+
+fromMilliseconds :: Double -> TimeDelta
+fromMilliseconds = TimeDelta . (/ 1000)
 
 toSeconds :: TimeDelta -> Double
 toSeconds (TimeDelta t) = t
