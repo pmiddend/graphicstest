e7574af2b305410fb5cdeaff64590938ddf22ea5
Philipp Middendorf <pmidden@secure.mailbox.org>
2015-06-26 10:56:11 +0200

    Ignore empty media dirs

--- src/Wrench/Filesystem.hs
+++ src/Wrench/Filesystem.hs
@@ -4,15 +4,20 @@ module Wrench.Filesystem(
   ) where
 
 import           ClassyPrelude hiding(FilePath,(</>))
-import           System.Directory (doesFileExist, getDirectoryContents)
+import           System.Directory ({-doesFileExist,-} getDirectoryContents,doesDirectoryExist)
 import System.FilePath
 import           Control.Category          ((>>>))
 
 getDirectoryContentsWrapped :: MonadIO m => FilePath -> m [FilePath]
 getDirectoryContentsWrapped fp = liftIO (getDirectoryContents fp)
 
+                                 {-
 doesFileExistWrapped :: MonadIO m => FilePath -> m Bool
 doesFileExistWrapped fp = liftIO (doesFileExist fp)
+-}
+
+doesDirectoryExistWrapped :: MonadIO m => FilePath -> m Bool
+doesDirectoryExistWrapped fp = liftIO (doesDirectoryExist fp)
 
 type Extension = String
 
@@ -22,5 +27,7 @@ getFilesWithExtInDir dir ext = filter (takeExtension >>> (== ext)) <$> getFilesI
 -- Holt nur die Files (ohne . und ..) aus einem Verzeichnis
 getFilesInDir :: MonadIO m => FilePath -> m [FilePath]
 getFilesInDir dir = do
-  result <- getDirectoryContentsWrapped dir >>= (return . map (dir </>)) >>= filterM doesFileExistWrapped
-  return result
+  dexists <- doesDirectoryExistWrapped dir
+  if dexists
+    then liftM (map (dir </>)) (getDirectoryContentsWrapped dir)
+    else return []
