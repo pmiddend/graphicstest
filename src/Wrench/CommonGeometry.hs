98eaa0e465597b9f895d03488021b8954970cf87
Philipp Middendorf <pmidden@secure.mailbox.org>
2015-07-21 21:37:36 +0200

    Add more boolean point operations

--- src/Wrench/CommonGeometry.hs
+++ src/Wrench/CommonGeometry.hs
@@ -1,11 +1,11 @@
 module Wrench.CommonGeometry where
 
-import Wrench.Rectangle
-import Wrench.FloatType
-import Wrench.Point
-import Linear.V2
-import Control.Lens((^.))
-import ClassyPrelude
+import           ClassyPrelude
+import           Control.Lens     ((^.))
+import           Linear.V2
+import           Wrench.FloatType
+import           Wrench.Point
+import           Wrench.Rectangle
 
 pointOp :: (FloatType -> FloatType -> Bool) -> Point -> Point -> Bool
 pointOp f (V2 x1 y1) (V2 x2 y2) = x1 `f` x2 && y1 `f` y2
@@ -15,6 +15,12 @@ pointGE a b = pointOp (>=) a b
 
 pointLE :: Point -> Point -> Bool
 pointLE a b = pointOp (<=) a b
+
+pointL :: Point -> Point -> Bool
+pointL a b = pointOp (<) a b
+
+pointG :: Point -> Point -> Bool
+pointG a b = pointOp (>) a b
 
 pointInRectangle :: Point -> Rectangle -> Bool
 pointInRectangle point rect = point `pointGE` (rect ^. rectLeftTop) && point `pointLE` (rect ^. rectRightBottom)
