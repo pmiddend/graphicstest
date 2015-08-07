8e730ea5249f0031be5321274e797e679a3b827d
Philipp Middendorf <pmidden@secure.mailbox.org>
2015-08-07 09:25:24 +0200

    haddock

--- src/Wrench/RenderBlockMode.hs
+++ src/Wrench/RenderBlockMode.hs
@@ -1,3 +1,8 @@
+{-|
+Module      : Wrench.RenderBlockMode
+Description : Data type to specify the target frame rate
+Maintainer  : pmidden@secure.mailbox.org
+-}
 {-# LANGUAGE TemplateHaskell #-}
 module Wrench.RenderBlockMode where
 
@@ -5,8 +10,8 @@ import Wrench.RenderTargetFps
 import Control.Lens(makePrisms)
 import ClassyPrelude
 
-data RenderBlockMode = RenderAndReturn
-                     | RenderAndWait RenderTargetFps
+data RenderBlockMode = RenderAndReturn -- ^ Render and return immediately
+                     | RenderAndWait RenderTargetFps -- ^ Render and suspend to reach the number of frames specified
                      deriving(Show,Eq)
 
 $(makePrisms ''RenderBlockMode)
--- src/Wrench/RenderTargetFps.hs
+++ src/Wrench/RenderTargetFps.hs
@@ -1,3 +1,8 @@
+{-|
+Module      : Wrench.RenderTargetFps
+Description : Type synonym to specify the target frames per second for rendering
+Maintainer  : pmidden@secure.mailbox.org
+-}
 module Wrench.RenderTargetFps where
 
 import ClassyPrelude
