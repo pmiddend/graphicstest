1ba646663906e8c8076bd0fddd75e63f3e24a20a
Philipp Middendorf <pmidden@secure.mailbox.org>
2015-08-09 11:23:21 +0200

    haddock streak continues

--- src/Wrench/ImageIdentifier.hs
+++ src/Wrench/ImageIdentifier.hs
@@ -1,3 +1,8 @@
+{-|
+Module      : Wrench.ImageIdentifier
+Description : Type synonym for textual image identifiers
+Maintainer  : pmidden@secure.mailbox.org
+-}
 module Wrench.ImageIdentifier where
 
 import           ClassyPrelude
--- src/Wrench/ImageMap.hs
+++ src/Wrench/ImageMap.hs
@@ -1,3 +1,8 @@
+{-|
+Module      : Wrench.ImageMap
+Description : Map images to their atlased components
+Maintainer  : pmidden@secure.mailbox.org
+-}
 module Wrench.ImageMap where
 
 import           ClassyPrelude
@@ -5,4 +10,5 @@ import qualified Data.Map.Strict        as M
 import           Wrench.ImageIdentifier
 import           Wrench.Rectangle
 
+-- ^ Map a sub-rectangle inside an image
 type ImageMap = M.Map ImageIdentifier (Rectangle Int)
